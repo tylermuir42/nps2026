60f2f1498f0e00ebdf9c5ed93a8d1c04bf41d7bf
Tyler Muir <tylermuir42@gmail.com>
2026-01-24 22:44:16 +0900

    completed part 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,27 @@
-import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
-const parkData = getParkData();
-
-function setParkIntro(data){
-    const introEl = document.querySelector(".intro");
-    introEl.innerHTML = `<h1>${parkData.fullName}</h1>
-    <p>${parkData.description}</p>`;
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
+  <p>${data.description}</p>`;
 }
 
-function setParkInfoLinks(data){
-    const infoEl = document.querySelector(".info");
-    const html = data.map(mediaCardTemplate);
-    infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
+  const html = data.map(mediaCardTemplate);
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-setHeaderFooter(parkData);
-setParkIntro(parkData);
-setParkInfoLinks(parkInfoLinks);
+async function init() {
+  const parkData = await getParkData();
+  const links = getInfoLinks(parkData.images);
+  setHeaderFooter(parkData);
+  setParkIntro(parkData);
+  setParkInfoLinks(links);
+}
 
+init();
